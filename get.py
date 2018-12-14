9f66ec472e39f89aa6a6b128ecd00a9d5f0a3e38
caiguojing <1010784344@qq.com>
2018-12-14 16:16:36 +0800

    增加header参数的传递

--- get.py
+++ get.py
@@ -3,6 +3,9 @@ import requests
 # 基于requests的get请求
 if __name__ == '__main__':
     url = 'http://www.baidu.com'
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2626.106 Safari/537.36'}
+    response = requests.get(url,headers=headers)
+    # 请求百度首页没加headers请求的时候数据是不对的
     response = requests.get(url)
     print response #<Response [200]>
     # 输出有乱码，因为当我们使用response.text的时候，他的解码格式是request根据他的响应头部猜
--- post.py
+++ post.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     query_string = {'query':'人生苦短，我用pyhton',
     'from':'zh',
     'to':'en'}
-    response = requests.post(url,data = query_string)
+    headers = {'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4'}
+    # 没加headers请求的时候数据是不对的
+    response = requests.post(url,data = query_string,headers = headers)
     print response #<Response [200]>(虽然是200，但是服务器可能判断我们是一个爬虫依然不给我们响应)
     print response.content
 
